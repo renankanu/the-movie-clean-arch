545e8e9c55e9c9916dddf97d55fcf8df937f4069
renankanu <renankanu@gmail.com>
2022-09-19 13:25:20 -0300

    feature: created the data model movie, similar with test

---
+++ test/features/movie/data/models/movie_model_test.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:the_movie_clean_arch/features/movie/data/models/models.dart';
+import 'package:the_movie_clean_arch/features/movie/domain/entities/entities.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  const tMovieModel = MovieModel(
+    name: 'Movie Test',
+    image: '',
+    popularity: 92.1,
+    voteCount: 1000,
+  );
+  test('should be a subclass of Movie Entity', () {
+    //assert
+    expect(tMovieModel, isA<MovieEntity>());
+  });
+
+  group('fromJson', () {
+    test('should return the movie model by JSON', () {
+      //arrange
+      final Map<String, dynamic> jsonMap =
+          json.decode(fixture('movie_model.json'));
+      //act
+      final result = MovieModel.fromJson(jsonMap);
+      //assert
+      expect(result, equals(tMovieModel));
+    });
+  });
+}
---
+++ test/features/movie/data/models/similar_model_test.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:the_movie_clean_arch/features/movie/data/models/similar_model.dart';
+import 'package:the_movie_clean_arch/features/movie/domain/entities/entities.dart';
+
+import '../../../../fixtures/fixture_reader.dart';
+
+void main() {
+  const tSimilarModel = [
+    SimilarModel(
+      name: 'Similar Test',
+      image: '',
+      description: 'Similar Description Test',
+    )
+  ];
+
+  test('should be a subclass of Similar Entity', () {
+    expect(tSimilarModel, isA<List<SimilarEntity>>());
+  });
+
+  group('fromJson', () {
+    test('should return the similar model by JSON', () {
+      //arrange
+      final jsonMap = json.decode(fixture('similar_model.json'));
+      //act
+      final result =
+          (jsonMap as List).map((e) => SimilarModel.fromJson(e)).toList();
+      //assert
+      expect(result, equals(tSimilarModel));
+    });
+  });
+}
